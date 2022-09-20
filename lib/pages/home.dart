b807d3c786ffed7276bf4f6dd579d726c72f81ff
arcadesArena <shreyanshu.dps@gmail.com>
2022-09-20 05:50:36 +0530

    fixed typecasting error

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,38 +10,10 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  TextEditingController _weightController = TextEditingController();
-  TextEditingController _heightController = TextEditingController();
-  dynamic _outputBMI = "BMI";
-  dynamic _outputCAT = "Body Mass Index";
-
-  void calculateBMI() {
-    double height = double.parse(_heightController.text) / 100;
-    double weight = double.parse(_weightController.text);
-
-    double heightSquare = height * height;
-    double bmi = weight / heightSquare;
-    _outputBMI = bmi;
-    if(bmi<18.5){
-      _outputCAT="Underweight";
-    }
-    else if(18.5<=bmi && bmi<=24.9){
-      _outputCAT="Normal";
-    }
-    else if(25.0<=bmi && bmi<=29.9){
-      _outputCAT="Overweight";
-    }
-    else if(30.0<=bmi && bmi<=34.9){
-      _outputCAT="Obese class I";
-    }
-    else if(35.0<=bmi && bmi<=39.9){
-      _outputCAT="Obese class II";
-    }
-    else if(40.0<=bmi){
-      _outputCAT="Obese class III";
-    }
-    setState(() {});
-  }
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  String _outputBMI = "";
+  String _outputCAT = "";
 
   @override
   Widget build(BuildContext context) {
@@ -51,74 +23,63 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(15.0,0,15.0,0),
+        padding: const EdgeInsets.fromLTRB(35.0,0,35.0,0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Column(
-              children: [
-                Text(_outputCAT,style: const TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
-                Text(_outputBMI,style: const TextStyle(fontSize: 70.0,fontWeight: FontWeight.bold),),
-              ],
-            ),
+            Text("Category $_outputCAT",style: const TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
+            Text("BMI $_outputBMI",style: const TextStyle(fontSize: 70.0,fontWeight: FontWeight.bold),),
             const SizedBox(height: 10,),
             TextField(
               controller: _weightController,
               decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.monitor_weight_outlined,color: Colors.blue),
                 labelText: "Weight (kg)",
-                border: UnderlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
+                border: UnderlineInputBorder(borderRadius: BorderRadius.circular(5.0),),
                 fillColor: Colors.blue[100],
                 filled: true,
               ),
               keyboardType: TextInputType.number,
-              // inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
             ),
             const SizedBox(height: 25,),
             TextField(
               controller: _heightController,
               decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.height),
                 labelText: "Height (cm)",
-                border: UnderlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
+                border: UnderlineInputBorder(borderRadius: BorderRadius.circular(5.0),),
                 fillColor: Colors.blue[100],
                 filled: true,
               ),
               keyboardType: TextInputType.number,
-              // inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly], // Only numbers
             ),
             const SizedBox(height: 25,),
             ElevatedButton(
-                onPressed: (){ calculateBMI;
-                  // setState(() {
-                  //   double ht = double.parse(_heightController.text).toDouble();
-                  //   double wt = double.parse(_weightController.text).toDouble();
-                  //   // double ht = (_heightController.text).toDouble();
-                  //   // double wt = (_weightController.text).toDouble();
-                  //   double bmi = (wt/(ht*ht));
-                  //   _outputBMI = bmi;
-                  //   if(bmi<18.5){
-                  //     _outputCAT="Underweight";
-                  //   }
-                  //   else if(18.5<=bmi && bmi<=24.9){
-                  //     _outputCAT="Normal";
-                  //   }
-                  //   else if(25.0<=bmi && bmi<=29.9){
-                  //     _outputCAT="Overweight";
-                  //   }
-                  //   else if(30.0<=bmi && bmi<=34.9){
-                  //     _outputCAT="Obese class I";
-                  //   }
-                  //   else if(35.0<=bmi && bmi<=39.9){
-                  //     _outputCAT="Obese class II";
-                  //   }
-                  //   else if(40.0<=bmi){
-                  //     _outputCAT="Obese class III";
-                  //   }
-                  // });
+                onPressed: (){
+                  double ht = double.parse(_heightController.text)/100;
+                  double wt = double.parse(_weightController.text);
+                  double bmi = (wt/(ht*ht));
+                  _outputBMI = bmi.toStringAsFixed(2);
+                  if(bmi<18.5){
+                    _outputCAT="Underweight";
+                  }
+                  else if(18.5<=bmi && bmi<=24.9){
+                    _outputCAT="Normal";
+                  }
+                  else if(25.0<=bmi && bmi<=29.9){
+                    _outputCAT="Overweight";
+                  }
+                  else if(30.0<=bmi && bmi<=34.9){
+                    _outputCAT="Obese class I";
+                  }
+                  else if(35.0<=bmi && bmi<=39.9){
+                    _outputCAT="Obese class II";
+                  }
+                  else if(40.0<=bmi){
+                    _outputCAT="Obese class III";
+                  }
+                  setState(() {});
                 },
                 child: const Padding(
                   padding: EdgeInsets.all(8.0),
